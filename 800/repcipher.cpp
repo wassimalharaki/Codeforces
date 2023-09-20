2fd739bf4d4470fd48205b90b0fdb7456007493e
Wassim Al Haraki <wassimalharaki@gmail.com>
2023-09-20 21:50:42 +0300

    11 new sols

---
+++ 800/repcipher.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define fast ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define int long long
+#define all(v) v.begin(), v.end()
+#define nl << '\n'
+#define v vector
+#define vi vector<int>
+#define pii pair<int, int>
+#define mp make_pair
+#define INF LONG_LONG_MAX
+#define F first
+#define S second
+
+int32_t main() {
+    fast
+
+    int n; cin >> n;
+
+    int find = 1;
+    int curr = 0;
+
+    for (int i = 0; i < n; i++) {
+        char c; cin >> c;
+        curr++;
+        if (curr == find) {
+            cout << c;
+            curr = 0;
+            find++;
+        }
+    }
+
+    return 0;
+}
