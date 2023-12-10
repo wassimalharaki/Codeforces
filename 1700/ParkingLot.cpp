a521dc1fc1dfbc1a50540741f4c085a3923baaec
Wassim Al Haraki <wassimalharaki@gmail.com>
2023-12-10 22:04:45 +0200

    1700 for fun

---
+++ 1700/ParkingLot.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define fast ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+#define int long long
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+#define nl << '\n'
+#define pii pair<int, int>
+#define v vector
+#define vi v<int>
+#define vvi v<vi>
+#define vpii v<pii>
+#define mp make_pair
+#define pb push_back
+#define INF LONG_LONG_MAX
+#define F first
+#define S second
+
+int binpow(int a, int b) {
+    int res = 1;
+    while (b > 0) {
+        if (b & 1)
+            res *= a;
+        a *= a;
+        b >>= 1;
+    }
+    return res;
+}
+
+int32_t main() {
+    fast;
+
+    int n; cin >> n;
+    cout << 2 * 3 * binpow(4, n - 2) + (n - 3) * 9 * binpow(4, n - 3);
+
+    return 0;
+}
